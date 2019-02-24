f7e89349bcc7b4a54430137a6cbab2852027cc83
daniel <verdudaniel7@gmail.com>
2019-02-24 18:01:33 +0100

    instalacion de Gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', ()=>
+    gulp.src('./scss/*.scss')
+    .pipe(sass({
+        outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('./css'))
+);
+
+gulp.task('watch', ()=> {
+    gulp.watch('./scss/*.scss', gulp.series('sass'))
+  });
+
+    
